65297b05b8157c08699a9308f388d7902396e589
scottkoh <scottkohxingzheng@gmail.com>
2023-02-17 15:04:07 +0800

    initial baseline for project

--- products/filters.py
+++ products/filters.py
@@ -5,7 +5,7 @@ from .models import *
 #filter for invoice generator
 
 class OrderFilter(django_filters.FilterSet):
-    start_date = DateFilter(field_name='transactionTime', lookup_expr='gte')
+    start_date= DateFilter(field_name='transactionTime', lookup_expr='gte')
     end_date = DateFilter(field_name='transactionTime', lookup_expr='lte')
     class Meta:
         model = Order
