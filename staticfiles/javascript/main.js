1f5b843f364bd1812980e8a0ea659eafe0f73ce4
scottkoh <scottkohxingzheng@gmail.com>
2023-02-24 12:46:30 +0800

    this is our first production release

--- staticfiles/javascript/main.js
+++ staticfiles/javascript/main.js
@@ -1,30 +1,5 @@
-
-    //getting searchForm and pageLinks
-    let searchForm = document.getElementById('searchForm')
-    let pageLinks = document.getElementsByClassName('page-link')
-
-    //ensure searchForm exists
-    if(searchForm){
-        for(let i=0; pageLinks.length >i; i++){
-            pageLinks[i].addEventListener('click', function (event) {
-                event.preventDefault()
-
-                //get the data attribute
-                let page = this.dataset.page
-                console.log('PAGE: ', page)
-
-                //add hidden search input to form
-                searchForm.innerHTML += `<input value=${page} name="page" hidden/>`
-
-                //submit form
-                searchForm.submit()
-            })
-        }
-    }
-
-
-var checkboxes = document.querySelectorAll("input[type = 'checkbox']");
-function checkAll(myCheckbox){
+let checkboxes = document.querySelectorAll("input[type = 'checkbox']");
+function selectAllOrders(myCheckbox){
     if(myCheckbox.checked == true){
         checkboxes.forEach(function(checkbox){
             checkbox.checked = true;
@@ -36,3 +11,28 @@ function checkAll(myCheckbox){
         });
     }
 }
+
+//getting searchForm and pageLinks
+let searchForm = document.getElementById('searchForm')
+let pageLinks = document.getElementsByClassName('page-link')
+
+//ensure searchForm exists
+if(searchForm){
+    for(let i=0; pageLinks.length >i; i++){
+        pageLinks[i].addEventListener('click', function (event) {
+            event.preventDefault()
+
+            //get the data attribute
+            let page = this.dataset.page
+            console.log('PAGE: ', page)
+
+            //add hidden search input to form
+            searchForm.innerHTML += `<input value=${page} name="page" hidden/>`
+
+            //submit form
+            searchForm.submit()
+        })
+    }
+}
+
+
