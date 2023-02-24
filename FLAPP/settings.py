f21ac4ac47ee95cb7224049e6e7840b4416de7bc
scottkoh <scottkohxingzheng@gmail.com>
2023-02-24 22:28:50 +0800

    initial baseline for project

--- FLAPP/settings.py
+++ FLAPP/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-01#$csf(tp5pns!5$5zqb*$8p^4e)-zq8t+%p3w%gefr$o$vk9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'flapp.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'flapp-app.herokuapp.com', 'flapp.herokuapp.com']
 
 
 # Application definition
